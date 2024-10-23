fbb0fb1b2be2200c8dc926904ec78945ba09b302
Emanuel Guaráz <guarazjemanuel@gmail.com>
2024-10-23 14:10:08 -0300

    Update menú de opciones

--- segundocuatrimestre/evidencia3/app/main.py
+++ segundocuatrimestre/evidencia3/app/main.py
@@ -3,15 +3,19 @@ import ordenamiento
 
 def display_menu():
     while True:
-        print("-" * 10 ,"Menu Principal","-" * 44)
-        print("1. Ordenamiento de usuarios")
-        print("2. Buscar usuario")
-        print("3. Cargar registros pluviales de un año")
-        print("4. Ver graficos de registros pluviales")
-        print("5. Ir al menú de la evidencia 2")
-        print("6. Salir")
+        print("-" * 25 ,"Menu Principal","-" * 29)
+        print('''   
+                    1) Ordenar usuarios\n
+                    2) Buscar usuario\n
+                    3) Cargar registros pluviales de un año\n
+                    4) Ver graficos de registros pluviales\n
+                    5) Ir al menú de la evidencia 2\n
+                    6) Salir\n''')
+        
         print("-" * 70)
+
         option = (input("Ingrese una opcion: "))
+        
         print("-" * 70)
         
         if option == "1":
@@ -25,6 +29,7 @@ def display_menu():
         
         elif option == "4":
             pass
+        
         elif option == "5":
             display_menu_ev2()
 
