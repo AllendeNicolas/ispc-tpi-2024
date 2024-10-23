cb0a6f742097083f69a2e9c29b8a0a1630037ad8
Emanuel Guaráz <guarazjemanuel@gmail.com>
2024-10-23 12:46:33 -0300

    Create función ordenar_burbuja

--- segundocuatrimestre/evidencia3/app/ordenamiento.py
+++ segundocuatrimestre/evidencia3/app/ordenamiento.py
@@ -1,23 +1,52 @@
 from operator import itemgetter
+import pickle
+import pandas as pd
 
-
-
-
-# FUNCION DE ORDENAMIENTO CON METODO BURBUJA
-def ordenar_burbuja (lista):
-    for i in range(len(lista) - 1):
-
-        for j in range(len(lista) - 1):
-
-            if lista[j] > lista[j+1]:
-                lista[j], lista[j+1] = lista[j+1], lista[j]
+def ordenar():
+    print('''ORDENAR USUARIOS, ELIJA LA FORMA EN QUE DESEA ORDENARLO\n
+          1) Técnica propia (Burbuja)\n
+          2) Ordenar por Python\n''')
     
-    return lista
+    print('En la segunda opción usamos la función "sorted()" en lugar del metodo .sort, \nestamos trabajando con lista de listas y no encontramos la manera de ordenarlo con sort')
+    print("-" * 70)
 
+# FUNCION DE ORDENAMIENTO CON METODO BURBUJA HECHA POR NOSOTROS
+def ordenar_burbuja ():
+    try:
+        with open('usuarios.ispc', 'rb') as archivo:
+            df_usuarios = pd.DataFrame(pickle.load(archivo))
+            print('\ndf MODIFICADO\n')
+            print(df_usuarios)
+    
 
+        #CONVIERTE EL DATAFRAME EN UNA LISTA DE LISTAS
+        usuarios_cargados_lista = df_usuarios.values.tolist()
 
+    #EN CASO DE QUE NO ENCUENTRE COINCIDENCIA O NO EXISTA EL ARCHIVO MUESTRA MSJ DE ERROR
+    except:
+        print('El archivo usuarios.ispc o el usuario no existe')
+
+    else:
+
+        for i in range(len(usuarios_cargados_lista) - 1):
+
+            for j in range(len(usuarios_cargados_lista) - 1):
+
+                if usuarios_cargados_lista[j][0] > usuarios_cargados_lista[j+1][0]:
+                    usuarios_cargados_lista[j], usuarios_cargados_lista[j+1] = usuarios_cargados_lista[j+1], usuarios_cargados_lista[j]
+
+        df_usuarios = pd.DataFrame(usuarios_cargados_lista, columns=['username', 'password', 'email'])    
+        
+        print('\ndf MODIFICADO\n')
+        print(df_usuarios)
+    
+
+# FUNCION DE ORDENAMIENTO USANDO LIBRERIA DE PYTHON
 def ordenar_lista_de_listas(lista_de_listas):
-    
+
     sorted(lista_de_listas, key=itemgetter(1))
 
     return lista_de_listas
+
+
+ordenar_burbuja()
