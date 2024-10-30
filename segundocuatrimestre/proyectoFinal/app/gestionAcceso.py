fa11fe2e663b2bf30b48c1e22b3a02012ecf155f
Emanuel Guaráz <guarazjemanuel@gmail.com>
2024-10-30 17:08:06 -0300

    Update funcion registrarAcceso

--- segundocuatrimestre/proyectoFinal/app/gestionAcceso.py
+++ segundocuatrimestre/proyectoFinal/app/gestionAcceso.py
@@ -12,14 +12,24 @@ def cargar_usuarios():
     return usuarios
 
 # Función para registrar accesos
-def registrar_acceso(username):
-    acceso = pd.DataFrame({
-        'usuario': [username],
-        'acceso': [datetime.now().strftime('%Y-%m-%d %H:%M:%S')]
-    })
+def registrarAcceso(username):
     try:
-        with open('accesos.ispc', 'ab') as file:
-            pickle.dump(acceso, file)
+        with open('accesos.ispc', 'rb') as archivo:
+            df_accesos = pd.DataFrame(pickle.load(archivo))
+            
+    # SI DA ERROR ABRIR EL ARCHIVO CREA UN DF VACIO PARA CONTINUAR CON LA CARGA
+    except:
+        df_accesos = pd.DataFrame({'usuario': username, 'acceso': datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
+    
+
+    nuevo_acceso = {'usuario': username, 'acceso': datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+    
+    df_accesos = df_accesos._append(nuevo_acceso, ignore_index=True)
+    
+    try:
+        with open('accesos.ispc', 'wb') as archivo:
+            pickle.dump(df_accesos, archivo)
+    
     except Exception as e:
         print(f"Error al registrar acceso: {e}")
 
@@ -34,7 +44,7 @@ def control_acceso(username, password):
     usuarios = cargar_usuarios()
     if not usuarios.empty and ((usuarios['username'] == username) & (usuarios['password'] == password)).any():
         print("Bienvenido ha ingresado a la aplicación")
-        registrar_acceso(username)
+        registrarAcceso(username)
     else:
         print("Acceso denegado")
         registrar_intento_fallido(username, password)
@@ -45,3 +55,33 @@ if __name__ == "__main__":
     username = input("Ingrese su nombre de usuario: ")
     password = input("Ingrese su contraseña: ")
     control_acceso(username, password)'''
+
+def menuDatosAccesos():
+    '''
+    Muestra el menú de datos de acceso y las opciones disponibles
+    '''
+    while True:
+        print("-" * 30 ,"DATOS DE ACCESOS","-" * 34)
+
+        print('''   
+                    1) Mostrar accesos\n
+                    2) Mostrar los logs de intentos fallidos\n
+                    3) Volver al menú anterior\n''')
+        
+        print("-" * 80)
+
+        option = (input("Ingrese una opción: "))
+        
+        print("-" * 80)
+
+       
+        if option == "1":
+            pass
+        
+        elif option == "2":
+            pass
+        
+        elif option == "3":
+            break
+        else:
+            print("Opción inválida, intente nuevamente.")
