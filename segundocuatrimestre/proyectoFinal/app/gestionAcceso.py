07bede898868b2035924bce2e2e77f0d5459fa50
Emanuel Guaráz <guarazjemanuel@gmail.com>
2024-10-30 17:49:29 -0300

    Update gestionAcceso - Se crea ffncion mostrarLogsFallidos

--- segundocuatrimestre/proyectoFinal/app/gestionAcceso.py
+++ segundocuatrimestre/proyectoFinal/app/gestionAcceso.py
@@ -2,11 +2,27 @@ import pandas as pd
 import pickle
 from datetime import datetime
 
+# APLICAR LA CLASE A LOS REGISTROS DE ACCESOS Y MODIFCAR ATRIBUTOS A LOS QUE SE USAN EN LA APP
+class Acceso:
+    def __init__(self, fecha_ingreso, fecha_salida, usuario_logueado):
+        self.fecha_ingreso = fecha_ingreso
+        self.fecha_salida = fecha_salida
+        self.__usuario_logueado = usuario_logueado
+
+    # MODIFICA ATRIBUTO PRIVADO usuario_logueado
+    def set_usuario_logueado(self, usuario):
+        self.__usuario_logueado = usuario
+    
+    # RETORNA ATRIBUTO PRIVADO usuario_logueado
+    def get_usuario_logueado(self):
+        return self.__usuario_logueado
+    
+
 # Función para cargar usuarios desde el archivo binario
 def cargar_usuarios():
     try:
-        with open('usuarios.ispc', 'rb') as file:
-            usuarios = pickle.load(file)
+        with open('usuarios.ispc', 'rb') as archivo:
+            usuarios = pickle.load(archivo)
     except FileNotFoundError:
         usuarios = pd.DataFrame(columns=['username', 'password'])
     return usuarios
@@ -19,7 +35,7 @@ def registrarAcceso(username):
             
     # SI DA ERROR ABRIR EL ARCHIVO CREA UN DF VACIO PARA CONTINUAR CON LA CARGA
     except:
-        df_accesos = pd.DataFrame({'usuario': username, 'acceso': datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
+        df_accesos = pd.DataFrame({'usuario': [], 'acceso': []})
     
 
     nuevo_acceso = {'usuario': username, 'acceso': datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
@@ -49,12 +65,27 @@ def control_acceso(username, password):
         print("Acceso denegado")
         registrar_intento_fallido(username, password)
 
-'''# Pruebas de uso de aplicación
-if __name__ == "__main__":
-    # Datos de ingreso del usuario
-    username = input("Ingrese su nombre de usuario: ")
-    password = input("Ingrese su contraseña: ")
-    control_acceso(username, password)'''
+def mostrarAccesos():
+    try:
+        with open('accesos.ispc', 'rb') as archivo:
+            accesos = pickle.load(archivo)
+    except FileNotFoundError:
+        print('El archivo no existe u ocurrió un error.')
+
+    else:
+        print("-" * 30 ,"REGISTROS DE ACCESOS","-" * 34)
+        print(accesos)
+
+
+def mostrarLogsFallidos():
+    try:
+        with open('logs.txt', 'r') as archivo:
+            print("-" * 30 ,"INTENTOS FALLIDOS","-" * 34)
+            for linea in archivo:
+                print(linea)
+    except FileNotFoundError:
+        print('El archivo no existe u ocurrió un error.')
+
 
 def menuDatosAccesos():
     '''
@@ -76,8 +107,8 @@ def menuDatosAccesos():
 
        
         if option == "1":
-            pass
-        
+            mostrarAccesos()
+    
         elif option == "2":
             pass
         
